a9f79285d308614b7e39078cdc9fcfe529eee497
Leighton Ho <l.ho@hotmail.co.nz>
2020-08-07 23:52:05 +1200

    Add project files.

---
+++ TranslateOCR.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TranslateOCR
+{
+	partial class TranslateOCR
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Capture = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// Capture
+			// 
+			this.Capture.Location = new System.Drawing.Point(21, 21);
+			this.Capture.Name = "Capture";
+			this.Capture.Size = new System.Drawing.Size(127, 28);
+			this.Capture.TabIndex = 0;
+			this.Capture.Text = "Capture";
+			this.Capture.UseVisualStyleBackColor = true;
+			this.Capture.Click += new System.EventHandler(this.Capture_Click);
+			// 
+			// TranslateOCR
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Capture);
+			this.Name = "TranslateOCR";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button Capture;
+	}
+}
+
---
+++ TranslateOCR.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace TranslateOCR
+{
+    public partial class TranslateOCR : Form
+	{
+		private System.ComponentModel.IContainer components = null;
+        private NotifyIcon notifyIcon;
+
+		public TranslateOCR()
+		{
+			InitializeComponent();
+
+            this.components = new System.ComponentModel.Container();
+
+            // Set up how the form should be displayed.
+            this.Hide();
+            this.Text = "Translate OCR";
+
+            // Create the NotifyIcon.
+            this.notifyIcon = new NotifyIcon(this.components);
+
+            // The Icon property sets the icon that will appear
+            // in the systray for this application.
+            notifyIcon.Icon = new Icon("language.ico");
+
+            // The ContextMenu property sets the menu that will
+            // appear when the systray icon is right clicked.
+            //notifyIcon.ContextMenu = this.contextMenu1;
+
+            // The Text property sets the text that will be displayed,
+            // in a tooltip, when the mouse hovers over the systray icon.
+            notifyIcon.Text = "Translate OCR";
+            notifyIcon.Visible = true;
+
+            // Handle the DoubleClick event to activate the form.
+            //notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+        }
+
+		private void Capture_Click(object sender, System.EventArgs e)
+		{
+            // Allow user to drag a rectangle on the screen.
+            Rectangle rect = Screen.GetBounds(Point.Empty);
+
+            string str = string.Empty;
+		}
+	}
+}
